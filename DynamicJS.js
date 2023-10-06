067ae2ff25bfacab0ff9c5e8bca02c090c13f7c9
DivyasimhaReddy <118110401+DivyasimhaReddy@users.noreply.github.com>
2023-10-06 22:03:37 +0530

    Add files via upload

---
+++ DynamicJS.js
@@ -0,0 +1,13 @@
+function switchoff() {
+    document.getElementById("bulbImage").src="https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/bulb-go-off-img.png";
+    document.getElementById("catImage").src="https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/cat-eyes-img.png";
+    document.getElementById("onSwitch").style.backgroundColor = "#22c55e";
+     document.getElementById("switchStatus").textContent = "Switched Off";
+    
+}
+function switchon(){
+    document.getElementById("bulbImage").src="https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/bulb-go-on-img.png";
+    document.getElementById("catImage").src="https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/cat-img.png"
+    document.getElementById("offSwitch").style.backgroundColor = "#e12d39";
+     document.getElementById("switchStatus").textContent = "Switched On";
+}
